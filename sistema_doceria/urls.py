b677adfaad40a2b9b0ac18088cdfd45a439f3a47
rafaelaWolffEvangelista <rafaelawolffzimermannevangelis@gmail.com>
2026-08-31 16:24:00 -0300

    fazendo urls

--- sistema_doceria/urls.py
+++ sistema_doceria/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from administradora.views import home,sobre,contato
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('', include('administradora.urls'))
 ]
